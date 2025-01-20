e6c2261684f54abc1ed2423fb9f2f9ea9a25801d
初墨似雪 <1182323879@qq.com>
2025-01-20 16:15:12 +0800

    着色器

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,14 +1,14 @@
-return{
-	{
-    "yelog/marklive.nvim",
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    lazy = true,
-    ft = "markdown",
-    opts = {}
-	},
-	{
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-},
+return {
+    {
+        "yelog/marklive.nvim",
+        dependencies = { 'nvim-treesitter/nvim-treesitter' },
+        lazy = true,
+        ft = "markdown",
+        opts = {}
+    },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+    },
 }
