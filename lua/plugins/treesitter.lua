c74f0353a83e4f7dcb8279fa877351f1b90bef14
初墨似雪 <1182323879@qq.com>
2025-01-20 15:58:15 +0800

    减少解析器

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	config = function()
 	require'nvim-treesitter.configs'.setup {
-		ensure_installed = "all",
+		ensure_installed = { "c", "bash", "lua" },
 		auto_install = true,
 		highlight = { enable = true, },
 		indent = { enable = true },
